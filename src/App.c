c287755e430d6a1604fd639868359bc1215ecda7
Cameron Henderson <CamGHenderson213@gmail.com>
2026-03-24 21:44:05 -0600

    upload

--- src/App.c
+++ src/App.c
@@ -1,24 +1,60 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <pigpio.h>
 #include <unistd.h>
 
-int32_t main()
+#define INDICATOR_LED 18
+
+void initialize()
 {
-    printf("%s\n", "Initializing Telemetry Program");
+    printf("%s\n", "Initializing Telemetry Program...");
     if(gpioInitialise() < 0) 
     {                                                                                                            
-        printf("%s\n", "gpio failed to initialize");                                                         
+        printf("%s\n", "gpio failed to initialize!");                                                         
         exit(-1);
     }                                                                                    
     
-    gpioSetMode(18, PI_OUTPUT);
-    gpioWrite(18, 1);
-    sleep(3);
-    gpioWrite(18, 0);
+    gpioSetMode(INDICATOR_LED, PI_OUTPUT);
+    gpioWrite(INDICATOR_LED, 1);
 
-    printf("%s\n", "Terminating Telemetry Program");
+    printf("%s\n", "Telemetry Program on Standby...");
+}
+
+void terminate()
+{
+    printf("%s\n", "Terminating Telemetry Program...");
+    gpioWrite(INDICATOR_LED, 0);
+    gpioTerminate();
+    printf("%s\n", "Telemetry Program Terminated.");
+
+}
+
+int32_t main()
+{
+    initialize();
+    
+    while(true)
+    {
+        char command[50];
+        scanf("%s", &command);
+
+        if(!strcmp(command, "record"))
+        {
+            printf("recording...");
+        }
+        else if(!strcmp(command, "stop-recording"))
+        {
+            printf("stoped recording.");
+        }
+        else if(!strcmp(command, "exit"))
+        {
+            break;
+        }
+    }
+    
+    terminate();
 
     return 0;
 }
