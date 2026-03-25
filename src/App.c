31260844db20ec39088e2ce3d434650d5fd49f33
Cameron Henderson <CamGHenderson213@gmail.com>
2026-03-24 21:29:29 -0600

    updated app.c

--- src/App.c
+++ src/App.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <pigpio.h>
+#include <unistd.h>
 
 int32_t main()
 {
-    printf("%s\n", "kill yourself now!");
+    printf("%s\n", "Initializing Telemetry Program");
+    if(gpioInitialise() < 0) 
+    {                                                                                                            
+        printf("%s\n", "gpio failed to initialize");                                                         
+        exit(-1);
+    }                                                                                    
+    
+    gpioSetMode(18, PI_OUTPUT);
+    gpioWrite(18, 1);
+    sleep(3);
+    gpioWrite(18, 0);
+
+    printf("%s\n", "Terminating Telemetry Program");
+
     return 0;
 }
