8e46b9b5677f16188574553ef12a1d9bae123150
Cameron Henderson <CamGHenderson213@gmail.com>
2026-03-28 03:56:22 -0600

    upload

--- src/App.c
+++ src/App.c
@@ -127,9 +127,18 @@ void terminate()
     printf("%s\n", "Telemetry Program Terminated.");
 }
 
+char* concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+    // in real code you would check for errors in malloc here
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
 void writeDataToFile()
 {
-    FILE* file = fopen(workingFileName, "w");
+    FILE* file = fopen(concat("data", workingFileName), "w");
     for(uint16_t i = 0; i < length; i++)
     {
         fprintf(file, "%.3f", data[i].time);
